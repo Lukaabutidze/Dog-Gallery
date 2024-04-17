73d44167b93c353dbeef55fbe9d0f4c050733886
Luka Abutidze <lukaabutidze99@gmail.com>
2024-04-17 15:50:05 +0400

    config fix for images

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Selector from "./components/Selector";
 import Button from "./components/Button";
 import { fetchDogs } from "./actions";
+import DogGallery from "./components/DogGallery";
 
 const DogsPage = () => {
   const [numberOfDogs, setNumberOfDogs] = useState(1);
@@ -13,8 +14,6 @@ const DogsPage = () => {
     setDogImages(await fetchDogs(numberOfDogs));
   };
 
-  console.log({ dogImages });
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-white dark:bg-gray-800">
       <Selector
@@ -23,6 +22,7 @@ const DogsPage = () => {
         value={numberOfDogs}
       />
       <Button buttonText="Fetch!" handleClick={handleFetch} />
+      <DogGallery dogImages={dogImages} />
     </div>
   );
 };
